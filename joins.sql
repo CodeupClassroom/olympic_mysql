472398457e03a6dff15ed3b25632e2bb3ab3ece0
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-04-06 16:32:52 -0500

    Add exercise write-ups for SQL joins and relationships

---
+++ joins.sql
@@ -0,0 +1,23 @@
+USE employees;
+
+-- Show hows each department along with the name of the current manager for that department.
+
+-- Find the name of all departments currently managed by women.
+
+-- Find the current titles of employees currently working in the Customer Service department.
+
+-- Find the current salary of all current managers.
+
+-- Find the names of all current employees, their department name, and their current manager's name.
+
+-- Historic average salary of female managers vs. male managers
+
+-- Current average salary of female managers vs. male managers
+
+-- Historic average salary of female workers vs. male workers
+
+-- Current average salary of female workers vs. male workers
+
+-- Current count of female managers vs male managers
+
+-- Current count of female engineers vs. male engineers
